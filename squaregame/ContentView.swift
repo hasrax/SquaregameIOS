e72be771c237ef9ea2dafc2af2e8fc10f52bd971
hasrax <hasragunawardena093@gmail.com>
2026-01-10 13:37:06 +0530

    Initial Commit

---
+++ squaregame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  squaregame
+//
+//  Created by COBSCCOMP24.2P-023 on 2026-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ squaregame/squaregameApp.swift
@@ -0,0 +1,17 @@
+//
+//  squaregameApp.swift
+//  squaregame
+//
+//  Created by COBSCCOMP24.2P-023 on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct squaregameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
